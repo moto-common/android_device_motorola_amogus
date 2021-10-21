01ebb4c6d16324e0ad15b09ea1cc8c0f1abc28c2
erfanoabdi <erfangplus@gmail.com>
2021-10-21 02:35:44 +0000

    amogus: Remake libgbp_shim
    Signed-off-by: electimon <electimon@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,5 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libgbp_shim.cpp
+LOCAL_SHARED_LIBRARIES := libgui_vendor libutils
 LOCAL_MODULE := libgbp_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
