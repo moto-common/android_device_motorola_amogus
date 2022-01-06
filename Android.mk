696abde21002feb04d18fce8ed8f514a94c75356
electimon <electimon@gmail.com>
2022-01-06 11:36:22 +0000

    amogus: Redo audio configurations and add amogus_folders
    Signed-off-by: electimon <electimon@gmail.com>

--- Android.mk
+++ Android.mk
@@ -9,4 +9,13 @@ $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard $(PRODUCT_VENDOR_KERNEL_H
 	mkdir -p $@/include
 	cp -a $(PRODUCT_VENDOR_KERNEL_HEADERS)/. $@/include
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := amogus_folders
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)
+
+LOCAL_POST_INSTALL_CMD := mkdir -p \
+    $(TARGET_OUT_VENDOR)/etc/sensors/config
+
+include $(BUILD_PHONY_PACKAGE)
+
 endif
