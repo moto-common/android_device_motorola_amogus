b30fccf45538a5637598864d9ec909d8b666407f
electimon <electimon@gmail.com>
2022-01-10 15:48:04 +0000

    amogus: Enable APEX support
    * Now that device is stable
    * we should enable this
    
    Signed-off-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -321,6 +321,6 @@ PRODUCT_PACKAGES += \
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/common/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # include board vendor blobs
 $(call inherit-product-if-exists, vendor/motorola/amogus/amogus-vendor.mk)
