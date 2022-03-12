84ccc56ccbbbe237add763071cc64dc75652f193
electimon <electimon@gmail.com>
2022-03-12 06:47:24 +0000

    amogus: Inherit 4096 dalvik heap config
    Signed-off-by: electimon <electimon@gmail.com>

--- aosp_amogus.mk
+++ aosp_amogus.mk
@@ -14,7 +14,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/amogus/device.mk)
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := aosp_amogus
@@ -23,4 +23,3 @@ PRODUCT_MODEL := Moto G(8) Family (AOSP)
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_SHIPPING_API_LEVEL := 29
-
