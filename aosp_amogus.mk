280dedff7fd470e7c57c29357652cee54ea8cf36
electimon <electimon@gmail.com>
2022-09-27 13:42:21 +0800

    amogus: Inherit dalvik heap properties for 2GB RAM devices
    Signed-off-by: electimon <electimon@gmail.com>

--- aosp_amogus.mk
+++ aosp_amogus.mk
@@ -14,7 +14,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/amogus/device.mk)
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := aosp_amogus
