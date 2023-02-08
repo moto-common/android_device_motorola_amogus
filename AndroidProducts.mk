641a79e46b765d65e170d0d793eeb42690de801f
electimon <electimon@gmail.com>
2023-02-08 16:30:37 +0800

    amogus: Move targets to common targets directory
    Signed-off-by: electimon <electimon@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,8 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_amogus.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosp_amogus-eng \
-    aosp_amogus-userdebug
+PRODUCT_MAKEFILES := $(wildcard device/motorola/targets/devices/amogus/*.mk)
